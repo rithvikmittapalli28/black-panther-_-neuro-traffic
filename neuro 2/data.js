d3a42b1a1d0628dd8d2d5b574cd95640087120bb
rithvikmittapalli28 <mvrithvik230@gmail.com>
2026-04-28 22:44:18 +0530

    Update data.js

--- neuro 2/data.js
+++ neuro 2/data.js
@@ -1,34 +1,74 @@
 const hospitals = [
   {
-    name: "City Hospital",
+    name: "City General Hospital",
     lat: 12.9716,
-    lng: 77.5946,
+    lng: 77.5946, // MG Road
     specialties: ["general"],
     beds: 5,
     waitTime: 10
   },
   {
     name: "Heart Care Center",
-    lat: 12.9750,
-    lng: 77.6000,
+    lat: 12.9352,
+    lng: 77.6245, // Koramangala
     specialties: ["cardiac"],
     beds: 2,
     waitTime: 5
   },
   {
     name: "Neuro Hospital",
-    lat: 12.9800,
-    lng: 77.6100,
+    lat: 13.0350,
+    lng: 77.5970, // Hebbal
     specialties: ["neuro"],
     beds: 3,
     waitTime: 8
   },
   {
-    name: "Apollo Cardiac",
-    lat: 12.9850,
-    lng: 77.6150,
-    specialties: ["cardiac"],
+    name: "Mother Care Hospital",
+    lat: 12.9081,
+    lng: 77.6476, // HSR Layout
+    specialties: ["maternity"],
     beds: 4,
     waitTime: 6
+  },
+  {
+    name: "Trauma Center",
+    lat: 12.9141,
+    lng: 77.6100, // BTM Layout
+    specialties: ["trauma"],
+    beds: 6,
+    waitTime: 4
+  },
+  {
+    name: "Children Hospital",
+    lat: 12.9279,
+    lng: 77.6271, // Indiranagar
+    specialties: ["pediatric"],
+    beds: 3,
+    waitTime: 7
+  },
+  {
+    name: "Pulmo Care Hospital",
+    lat: 12.9767,
+    lng: 77.5713, // Rajajinagar
+    specialties: ["pulmonology"],
+    beds: 3,
+    waitTime: 6
+  },
+  {
+    name: "Urology Specialist Center",
+    lat: 12.9698,
+    lng: 77.7500, // Whitefield
+    specialties: ["urology"],
+    beds: 4,
+    waitTime: 7
+  },
+  {
+    name: "Global Multispeciality Hospital",
+    lat: 12.2958,
+    lng: 76.6394, // Mysore (far away fallback)
+    specialties: ["multispeciality"],
+    beds: 8,
+    waitTime: 5
   }
 ];
